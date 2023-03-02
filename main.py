b6695eb9f0385c20c6341e5bfd714a5fa1156911
git-gud-casual <popasena2@ya.ru>
2023-03-03 00:27:01 +0300

    Added test matrix

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import matrix
 import time
 
+import argparse
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+
+
+def get_int_list2_from_file(filename: str) -> list[list[int]]:
     a = []
-    with open("13.txt") as f:
+    with open(filename) as f:
         for line in f.readlines():
             a.append(list(map(int, line.split())))
+    return a
+
+
+if __name__ == '__main__':
+    """
+    5х5 - 704
+    10х10 - 194851613
+    13х13 - 72986555176
+    """
+    args = parser.parse_args()
+    arr = get_int_list2_from_file(args.filename)
 
     start_time = time.time()
-    result = matrix.determinant(a)
+    result = matrix.determinant(arr)
     final_time = time.time()
 
     print(result)
-    print(f"Time: {final_time - start_time} seconds")
+    print(f'Time: {final_time - start_time} seconds')
 
 
